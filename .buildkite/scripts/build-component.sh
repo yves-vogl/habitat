b06774942b44a71d57c11fef68ecd8bab84eb177
Christopher Maier <cmaier@chef.io>
2018-06-01 17:48:04 -0400

    Once more with feeling
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/scripts/build-component.sh
+++ .buildkite/scripts/build-component.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 component=${1}
 
-hab studio build "components/${component}"
+HAB_STUDIO_SUP=false hab studio run "build components/${component}"
 
 # TODO (CM): Push to Builder in a release channel
 # TODO (CM): pass a release channel in as an argument / environment variable
