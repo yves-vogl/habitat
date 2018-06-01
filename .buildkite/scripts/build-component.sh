0bd19da66276ef46c3d21e882e3666994fea02ed
Christopher Maier <cmaier@chef.io>
2018-06-01 17:33:55 -0400

    sudo

--- .buildkite/scripts/build-component.sh
+++ .buildkite/scripts/build-component.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 
 component=${1}
 
-hab pkg build "components/${component}"
+sudo hab pkg build "components/${component}"
 
 # TODO (CM): Push to Builder in a release channel
 # TODO (CM): pass a release channel in as an argument / environment variable
 # TODO (CM): pass an auth token in environment
+# TODO (CM): Keep the HART as an artifact in Buildkite?
