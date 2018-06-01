64b59f866f444e25ada6b5347efb79212fe3f917
Christopher Maier <cmaier@chef.io>
2018-06-01 17:44:06 -0400

    TEMPORARY: build w/ studio
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/scripts/build-component.sh
+++ .buildkite/scripts/build-component.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 component=${1}
 
-sudo hab pkg build "components/${component}"
+hab studio build "components/${component}"
 
 # TODO (CM): Push to Builder in a release channel
 # TODO (CM): pass a release channel in as an argument / environment variable
