0d138d9251cb149941aa9d33a92c0bf3c42d234d
Christopher Maier <cmaier@chef.io>
2018-06-04 10:51:55 -0400

    split out success and failure logic to sub-pipelines
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/scripts/build-component.sh
+++ .buildkite/scripts/build-component.sh
@@ -4,9 +4,17 @@ set -euo pipefail
 
 component=${1}
 
-HAB_STUDIO_SUP=false hab studio run "build components/${component}"
+hab pkg build "components/${component}"
 
 # TODO (CM): Push to Builder in a release channel
 # TODO (CM): pass a release channel in as an argument / environment variable
 # TODO (CM): pass an auth token in environment
 # TODO (CM): Keep the HART as an artifact in Buildkite?
+#            Might be a good way to ensure that we've got the hab
+#            version we just built
+# TODO (CM): pass in an origin key and import it
+
+
+# In Travis, we had to have logic for pushing depending on whether
+# we're doing a release build or not. Since we can construct the
+# pipeline however we want here, we can have simpler scripts
