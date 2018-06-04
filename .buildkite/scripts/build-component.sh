cd9b529096c71a873cf3ca83d03d35ab3fb6100f
Christopher Maier <cmaier@chef.io>
2018-06-04 13:59:54 -0400

    unset binlink dir
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/scripts/build-component.sh
+++ .buildkite/scripts/build-component.sh
@@ -47,7 +47,8 @@ import_key
 # echo
 
 hab studio rm
-HAB_STUDIO_SECRET_HAB_BINLINK_DIR=$HOME/tools/bin HAB_NONINTERACTIVE=1 HAB_ORIGIN=core HAB_STUDIO_SUP=false hab studio run "build components/${component}"
+unset HAB_BINLINK_DIR
+HAB_NONINTERACTIVE=1 HAB_ORIGIN=core HAB_STUDIO_SUP=false hab studio run "build components/${component}"
 
 #hab pkg build "components/${component}"
 
