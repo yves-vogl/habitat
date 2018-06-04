f97b1c960235d07b57b774869f7adc88a53f1cb9
Christopher Maier <cmaier@chef.io>
2018-06-04 16:41:01 -0400

    echo -e
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 if [[ "${FAKE_RELEASE_TAG}" || "${BUILDKITE_TAG}" ]]; then
     # Our releases are currently triggered by the existence of a tag
-    echo "--- :sparkles: Preparing for a release! :sparkles:"
+    echo -e "--- :sparkles: Preparing for a release! :sparkles:"
 
     if [[ "${FAKE_RELEASE_TAG}" ]]; then
-        echo ":warning: Using fake release tag '${FAKE_RELEASE_TAG}' :warning:"
+        echo -e ":warning: Using fake release tag '${FAKE_RELEASE_TAG}' :warning:"
         channel="rc-${FAKE_RELEASE_TAG}"
     elif [[ "${BUILDKITE_TAG}" ]]; then
         echo "Using release tag '${BUILDKITE_TAG}'"
@@ -18,7 +18,7 @@ if [[ "${FAKE_RELEASE_TAG}" || "${BUILDKITE_TAG}" ]]; then
     buildkite-agent meta-data set "release-channel" "${channel}"
     buildkite-agent pipeline upload .buildkite/release_pipeline.yaml
 else
-    echo "--- :sparkles: Preparing for a CI run! :sparkles:"
+    echo -e "--- :sparkles: Preparing for a CI run! :sparkles:"
     echo "TODO"
     exit 1
 fi
