23c3fd7f7c9644b8fc6b2cd8be5f63fa4df5c41d
Christopher Maier <cmaier@chef.io>
2018-06-04 16:12:41 -0400

    Actually upload the pipeline
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -6,10 +6,10 @@ set -euo pipefail
     # Our releases are currently triggered by the existence of a tag
 
     # channel="rc-${BUILDKITE_TAG}"
-    channel="magical-mystery-tour"
-    echo "--- :sparkles: Preparing for a release! :sparkles:"
-    buildkite-agent meta-data set "release-channel" "${channel}"
-    buildkite-agent upload .buildkite/release_pipeline.yaml
+channel="magical-mystery-tour"
+echo "--- :sparkles: Preparing for a release! :sparkles:"
+buildkite-agent meta-data set "release-channel" "${channel}"
+buildkite-agent pipeline upload .buildkite/release_pipeline.yaml
 # else
 #     echo "--- :sparkles: Preparing for a CI run! :sparkles:"
 #     echo "TODO"
