9cc3916096e3ae4353e7f27895cb46c6d2d6b599
Christopher Maier <cmaier@chef.io>
2018-06-05 10:34:38 -0400

    Switch to new hab after studio build
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .buildkite/scripts/build_component.sh
+++ .buildkite/scripts/build_component.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+# TODO (CM): Consider setting HAB_NONINTERACTIVE in the top-level
+# environment, instead
+export HAB_NONINTERACTIVE=1
+
 # `component` should be the subdirectory name in `components` where a
 # particular component code resides.
 #
@@ -11,42 +15,83 @@ component=${1}
 
 channel=$(buildkite-agent meta-data get "release-channel")
 
+# This function _must_ be called first!
+set_habitat_binary
+import_keys
+
 # TODO (CM): Extract this function to somewhere else?
 import_keys() {
     echo "--- :key: Downloading 'core' public keys from Builder"
-    hab origin key download core
+    ${hab_binary} origin key download core
     echo "--- :closed_lock_with_key: Downloading latest 'core' secret key from Builder"
-    hab origin key download \
+    ${hab_binary} origin key download \
         --auth="${HAB_TEAM_AUTH_TOKEN}" \
         --secret \
         core
     # TODO (CM): delete the secret key later?
 }
-import_keys
+
+# Until we have built both a new core/hab _and_ a new core/hab-studio package, we
+# should continue to use the `hab` binary provided on our Buildkite
+# builders (managed by Release Engineering) (these should be the
+# latest stable release, btw).
+#
+# Once we have bootstrapped ourselves enough, however, we should
+# switch subsequent builds to use the new hab, which in turn uses the
+# new studio.
+set_hab_binary() {
+    local hab_ident=$(buildkite-agent meta-data get hab-version)
+    local studio_ident=$(buildkite-agent meta-data get studio-version)
+
+    echo "--- :thinking_face: Determining which `hab` binary to use"
+    if [[ "${hab_ident}" && "${studio_ident}" ]]; then
+        echo -e ":buildkite: metadata found; installing new versions of 'core/hab' and 'core/hab-studio'"
+        # By definition, these will be fully-qualified identifiers,
+        # and thus do not require a `--channel` option. However, they
+        # should be coming from the release channel, and should be the
+        # same packages built previously in this same release pipeline.
+
+        hab pkg install "${hab_ident}"
+        hab pkg install "${studio_ident}"
+        declare -g hab_binary="/hab/pkgs/${hab_ident}/bin/hab"
+    else
+        echo -e ":buildkite: metadata NOT found; using previously-installed hab binary"
+        declare -g hab_binary="$(which hab)"
+    fi
+    echo ":habicat: Using $(${hab_binary} --version)"
+}
 
 echo "--- :zap: Cleaning up old studio, if present"
-hab studio rm
+${hab_binary} studio rm
 
 echo "--- :habicat: Building components/${component}"
-unset HAB_BINLINK_DIR # This is set by releng, but seems to be messing
-# things up for us in the studio
+
+# The binlink dir is set by releng, but seems to be messing things up
+# for us in the studio.
+unset HAB_BINLINK_DIR
 export HAB_ORIGIN=core
-export HAB_NONINTERACTIVE=1
-HAB_BLDR_CHANNEL="${channel}" hab pkg build "components/${component}"
+
+HAB_BLDR_CHANNEL="${channel}" ${hab_binary} pkg build "components/${component}"
+
 source results/last_build.env
 
 case "${component}" in
     "hab")
         echo "--- :buildkite: Storing artifact ${pkg_ident}"
-        buildkite-agent artifact upload "results/${pkg_artifact}"
+        # buildkite-agent artifact upload "results/${pkg_artifact}"
         buildkite-agent meta-data set "hab-version" "${pkg_ident}"
+        ;;
+    "studio")
+        echo "--- :buildkite: Recording metadata for ${pkg_ident}"
+        # buildkite-agent artifact upload "results/${pkg_artifact}"
+        buildkite-agent meta-data set "studio-version" "${pkg_ident}"
         ;;
     *)
         ;;
 esac
 
 echo "--- :habicat: Uploading ${pkg_ident} to Builder in the '${channel}' channel"
-hab pkg upload \
+${hab_binary} pkg upload \
     --channel="${channel}" \
     --auth="${HAB_TEAM_AUTH_TOKEN}" \
     "results/${pkg_artifact}"
